e5b6b3f5f72bfe23af927cd1992cc95600cec05d
Maria <titenko_mm@mail.ru>
2026-04-12 21:31:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -184,8 +184,8 @@ if not st.session_state.authenticated:
             st.markdown('<h2 style="text-align:center;color:#002882;margin-bottom:5px;">Умная карта ВТБ</h2>', unsafe_allow_html=True)
             st.markdown("""
             <div style="text-align:center; padding: 15px; background: rgba(255,255,255,0.7); border-radius: 12px; margin-bottom: 15px; box-shadow: 0 2px 10px rgba(0,0,0,0.05);">
-            🗺️ Находите на карте выгодные предложения партнёров, подобранные специально для вас.<br/>
-            💳 Экономьте на повседневных покупках с персональными рекомендациями и кешбэком!
+            Находите на карте выгодные предложения партнёров, подобранные специально для Вас.<br/>
+            Экономьте на покупках с персональными рекомендациями и кешбэком!
             </div>
             """, unsafe_allow_html=True)
             with st.form("client_login_form"):
@@ -195,11 +195,10 @@ if not st.session_state.authenticated:
             with st.expander("Демо-доступ"): st.markdown("Логин для Марины: `bubliki`")
             
         else:
-            st.markdown('<h2 style="text-align:center;color:#002882;margin-bottom:5px;">ВТБ для бизнеса</h2>', unsafe_allow_html=True)
+            st.markdown('<h2 style="text-align:center;color:#002882;margin-bottom:5px;">ВТБ - банк для бизнеса</h2>', unsafe_allow_html=True)
             st.markdown("""
             <div style="text-align:center; padding: 15px; background: rgba(255,255,255,0.7); border-radius: 12px; margin-bottom: 15px; box-shadow: 0 2px 10px rgba(0,0,0,0.05);">
-            🚀 Привлекайте новых клиентов и увеличивайте выручку с сервисом «Умная карта» от ВТБ.<br/>
-            📊 Запускайте акции, анализируйте эффективность и растите вместе с нами.
+            Привлекайте новых клиентов и увеличивайте выручку с сервисом «Умная карта» от ВТБ.<br/>
             </div>
             """, unsafe_allow_html=True)
             with st.form("biz_login_form"):
